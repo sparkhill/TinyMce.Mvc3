c444ecf10fc71f335359a6ed88b558798945050b
Ryan Whitmire <ryanw51@gmail.com>
2012-04-27 00:39:34 -0400

    started img tag construction

--- TinyMce.Mvc3/Scripts/tinymce/plugins/mediapicker/js/dialog.js
+++ TinyMce.Mvc3/Scripts/tinymce/plugins/mediapicker/js/dialog.js
@@ -16,6 +16,34 @@ var MediaPickerDialog = {
 
     insertImage: function () {
         // Insert the contents from the input into the document
+        var src = $("#ImageUrl").val();
+        var alt = $("#AltText").val();
+        var cssClass = $("#CssClass").val();
+        var cssStyles = $("#CssStyles").val();
+        var alignment = $("#Alignment").val();
+        var width = $("#Width").val();
+        var height = $("#Height").val();
+        var img = "<img {src}{alt}{cssClass}{cssStyles}{width}{height}/>";
+
+        if (!src.isBlank()) {
+            src = 'src="{1}" '.assign(src);
+        }
+
+        if (!alt.isBlank()) {
+            alt = 'alt="{1}" '.assign(alt);
+        }
+
+        img = img.assign({
+            src: src,
+            alt: alt,
+            cssClass: cssClass,
+            cssStyles: cssStyles,
+            width: width,
+            height: height
+        });
+
+        alert(img);
+
         tinyMCEPopup.editor.execCommand('mceInsertContent', false, $("#ImageTag").val());
         tinyMCEPopup.close();
     },
